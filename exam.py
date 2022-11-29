65a9a22f8730d9cbc56290dff8f351015744c67c
aawazgautam <115046576+aawazgautam@users.noreply.github.com>
2022-11-29 17:19:06 -0600

    Add files via upload

--- exam.py
+++ exam.py
@@ -22,12 +22,13 @@ pi = 3.14  # constant value
 g = 32.2 # ft/s2
 constant= 16*q**2/(pi*cd**2*g)
 
+# Function to calculate the functional file.
 def fun (d):
     d= d**5 - 12 * d**4 +constant
     return d
-
+# Function to calculate the first derivative.
 def fun_fir(d1):
-    d1: 5*d1**4 - 48*d1**3
+    d1: 5*d1**4 - 48*d1**3 # manually calculated first derivative.
     return d1
 
 """
@@ -38,7 +39,7 @@ print("sqrt(2) =", np.sqrt(2))
 """
 
 d1n=0
-tol=1
+tol=1 # tolerance defined to enter the loop.
 while (tol > 0.0000000001):
         # First we save existing value of x,y&z to a variable and determine the more precise value of.
         d1= d1n
@@ -58,28 +59,31 @@ pop = pd.read_csv("C:\\Users\\aawaz\\Desktop\\exam\\data.csv") # importing the f
 
 # importing matplotlib library to plot line graph
 import matplotlib.pyplot as plt
+
+# Here had to create the list of populations because I wasn't able to read the valued from the excel file somehow.
 population=[308745538, 308758105, 309327143, 311583481, 313877662, 316059947, 318386329, 320738994, 323071755, 325122128, 326838199, 328329953, 329398742] 
 year =['2010 Apr', '2010 Jul', '2011','2012', '2013', '2014', '2015', '2016', '2017', '2018', '2019', '2020 Apr', '2020 Jul']
 
-plt.plot(year, population)
-plt.ylabel("Population in number")
-plt.title("Population of US 2010-2020")
+plt.plot(year, population) # plot x axis vs y axis
+plt.ylabel("Population in number")  # label for y axis
+plt.title("Population of US 2010-2020")  # Title of the plot
 plt.axis(year)
-plt.show()
+plt.show()  # command to show the plot
 
 
 
 # Problem 3 
 # Given condition a =L =25000cm
+# DEfiening cnsants
 
 # length of the cantilever
-l= 25000/100 #cm
+l= 25000/100 #m
 E= 200000 #pa = N/m2
 I= 81960000/pow(100,4) # m4
 # deflection due to uniform load:
 
 del_uni= q*l/(8*E*I))
-
+# Plan is to add the deflection due to uniform load and due to point load bu using the given equations
 
 
 
